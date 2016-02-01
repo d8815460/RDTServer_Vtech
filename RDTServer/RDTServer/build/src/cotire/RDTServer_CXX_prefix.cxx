6d17499a84df38e3dc4f0b141afc7b20290b2c5a
jakey <jakey_lee@tutk.com>
2016-02-01 13:48:42 +0800

    移除build的資料

--- RDTServer/RDTServer/build/src/cotire/RDTServer_CXX_prefix.cxx
+++
@@ -1,5 +0,0 @@
-/* cotire.cmake 1.7.6 generated file */
-/* /Users/jakey/Documents/SyncData/Repository/TUTK/RDTServer/RDTServer/RDTServer/build/src/cotire/RDTServer_CXX_prefix.cxx */
-#ifdef __cplusplus
-#include "/Users/jakey/Documents/SyncData/Repository/TUTK/RDTServer/RDTServer/RDTServer/src/PrefixHeader.pch"
-#endif
--- RDTServer/RDTServer/build/src/cotire/RDTServer_CXX_prefix.hxx
+++
@@ -1,5 +0,0 @@
-/* cotire.cmake 1.7.6 generated file */
-/* /Users/jakey/Documents/SyncData/Repository/TUTK/RDTServer/RDTServer/RDTServer/build/src/cotire/RDTServer_CXX_prefix.hxx */
-#ifdef __cplusplus
-#include "/Users/jakey/Documents/SyncData/Repository/TUTK/RDTServer/RDTServer/RDTServer/build/src/cotire/RDTServer_CXX_prefix.cxx"
-#endif
