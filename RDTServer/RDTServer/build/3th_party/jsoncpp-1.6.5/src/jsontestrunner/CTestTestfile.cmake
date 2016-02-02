ed383f36baa10e1efe4ac42ffdb2e5dcadce6a4c
baldwinli <baldwin_li@vtech.com>
2016-02-02 17:11:12 +0800

    we remove the content inside build folder

--- RDTServer/RDTServer/build/3th_party/jsoncpp-1.6.5/src/jsontestrunner/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/baldwin/test/tutk/RDTServer_Vtech/RDTServer/RDTServer/3th_party/jsoncpp-1.6.5/src/jsontestrunner
-# Build directory: /home/baldwin/test/tutk/RDTServer_Vtech/RDTServer/RDTServer/build/3th_party/jsoncpp-1.6.5/src/jsontestrunner
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- RDTServer/RDTServer/build/src/cotire/RDTServer_CXX_prefix.hxx
+++
@@ -1,5 +0,0 @@
-/* cotire.cmake 1.7.6 generated file */
-/* /home/baldwin/test/tutk/RDTServer_Vtech/RDTServer/RDTServer/build/src/cotire/RDTServer_CXX_prefix.hxx */
-#ifdef __cplusplus
-#include "/home/baldwin/test/tutk/RDTServer_Vtech/RDTServer/RDTServer/build/src/cotire/RDTServer_CXX_prefix.cxx"
-#endif
