9891d3bf595ca6ffb1134052957b0eb6bc32f304
jakey <jakey_lee@tutk.com>
2016-03-09 10:17:46 +0800

    新增databaseManager 移除sample for database

--- RDTServer/RDTServer/Targate/TUTKDevice/main.cpp
+++ RDTServer/RDTServer/Targate/TUTKDevice/main.cpp
@@ -294,41 +294,41 @@ int main(int argc, char *argv[])
     }
     
     {
-        int rows, cols;
-        sqlite3 *db;
-        char *errMsg = NULL;
-        char **result;
-        
-        /* 開啟 database 檔 */
-        if (sqlite3_open_v2("database.db3", &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL)) {
-            return -1;
-        }
-        
-        /* 建立 Table */
-        sqlite3_exec(db, createsql, 0, 0, &errMsg);
-        
-        /* 新增一筆資料 */
-        sqlite3_exec(db, insertsql, 0, 0, &errMsg);
-        /* 取得該筆資料的 ID */
-        printf("%lld\n", sqlite3_last_insert_rowid(db));
-        
-        /* 取得 database 裡所有的資料 */
-        sqlite3_get_table(db , querysql, &result, &rows, &cols, &errMsg);
-        
-        /* 列出所有資料 */
-        for (int i=0 ; i<rows ; i++) {
-            for (int j=0 ; j<cols ; j++) {
-                printf("%s\t", result[i * cols + j]);
-            }
-            
-            printf("\n");
-        }
-        
-        /* 釋放 */
-        sqlite3_free_table(result);
-        
-        /* 關閉 database */
-        sqlite3_close(db);
+//        int rows, cols;
+//        sqlite3 *db;
+//        char *errMsg = NULL;
+//        char **result;
+//        
+//        /* 開啟 database 檔 */
+//        if (sqlite3_open_v2("database.db3", &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL)) {
+//            return -1;
+//        }
+//        
+//        /* 建立 Table */
+//        sqlite3_exec(db, createsql, 0, 0, &errMsg);
+//        
+//        /* 新增一筆資料 */
+//        sqlite3_exec(db, insertsql, 0, 0, &errMsg);
+//        /* 取得該筆資料的 ID */
+//        printf("%lld\n", sqlite3_last_insert_rowid(db));
+//        
+//        /* 取得 database 裡所有的資料 */
+//        sqlite3_get_table(db , querysql, &result, &rows, &cols, &errMsg);
+//        
+//        /* 列出所有資料 */
+//        for (int i=0 ; i<rows ; i++) {
+//            for (int j=0 ; j<cols ; j++) {
+//                printf("%s\t", result[i * cols + j]);
+//            }
+//            
+//            printf("\n");
+//        }
+//        
+//        /* 釋放 */
+//        sqlite3_free_table(result);
+//        
+//        /* 關閉 database */
+//        sqlite3_close(db);
     }
     
     // RDT
