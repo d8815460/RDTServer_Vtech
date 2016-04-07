93c67e0398021b45d81c228bf527984ab87b3bbe
jakey <jakey_lee@tutk.com>
2016-04-07 13:58:49 +0800

    重構2

--- RDTServer/RDTServer/DeviceAPI/JsonRDTServerCommand.cpp
+++ RDTServer/RDTServer/DeviceAPI/JsonRDTServerCommand.cpp
@@ -137,10 +137,7 @@ void JsonRDTServerCommand::processCommandTarget(Json::Value& inJsonObject, Json:
     CommandBase* pCommandBase = NULL;
     
     // 讀取
-    bool readRunning = true;
     if (function.find("read") != std::string::npos) {
-        string SQL = "";
-        
         if (IfObject.isMember("AID")) {
             Json::Value jsonArray = IfObject["AID"];
             
@@ -158,10 +155,10 @@ void JsonRDTServerCommand::processCommandTarget(Json::Value& inJsonObject, Json:
                 pItems->dataType = DataType_Accessory;
                 pItems->pojoList = pojoList;
                 m_pCommandHardwardEvent->onCommandHardwardRecv_ReadItems(pItems);
-                
-                readRunning = false;
             }
             else {
+                string SQL = "";
+                
                 // 依照查詢條件生成Accessory
                 vector<ValueObject> voList;
                 for (int i=0 ; i<jsonArray.size() ; i++) {
@@ -170,57 +167,56 @@ void JsonRDTServerCommand::processCommandTarget(Json::Value& inJsonObject, Json:
                 }
                 
                 SQL = Pojo::genInSQL(voList, false);
-            }
-        }
-        
-        if (readRunning == true) {
-            if (IfObject.isMember("Element")) {
-                Json::Value jsonArray = IfObject["Element"];
                 
-                // 依照查詢條件生成
-                vector<ValueObject> voList;
-                for (int i=0 ; i<jsonArray.size() ; i++) {
-                    string Element = jsonArray[i].asString();
-                    voList.push_back(ValueObject("Element", Element));
+                if (IfObject.isMember("Element")) {
+                    Json::Value jsonArray = IfObject["Element"];
+                    
+                    // 依照查詢條件生成
+                    vector<ValueObject> voList;
+                    for (int i=0 ; i<jsonArray.size() ; i++) {
+                        string Element = jsonArray[i].asString();
+                        voList.push_back(ValueObject("Element", Element));
+                    }
+                    
+                    SQL.append(Pojo::genInSQL(voList, true));
                 }
                 
-                SQL.append(Pojo::genInSQL(voList, true));
-            }
-            
-            if (IfObject.isMember("ElementNO")) {
-                Json::Value jsonArray = IfObject["ElementNO"];
-                
-                // 依照查詢條件生成
-                vector<ValueObject> voList;
-                for (int i=0 ; i<jsonArray.size() ; i++) {
-                    int ElementNO = jsonArray[i].asInt();
-                    voList.push_back(ValueObject("ElementNO", ElementNO));
+                if (IfObject.isMember("ElementNO")) {
+                    Json::Value jsonArray = IfObject["ElementNO"];
+                    
+                    // 依照查詢條件生成
+                    vector<ValueObject> voList;
+                    for (int i=0 ; i<jsonArray.size() ; i++) {
+                        int ElementNO = jsonArray[i].asInt();
+                        voList.push_back(ValueObject("ElementNO", ElementNO));
+                    }
+                    
+                    SQL.append(Pojo::genInSQL(voList, true));
                 }
                 
-                SQL.append(Pojo::genInSQL(voList, true));
+                Json::Value json;
+                shared_ptr<vector<shared_ptr<Pojo>>> pojoList = AccessoryDao::readNestWithSQL(SQL);
+                
+                // 寫入至json輸出
+                Utility::pojoListToJson(inJsonObject, outJsonObject, pojoList);
+                
+                pCommandBase = new CommandHardwardRecv_ReadItems();
+                CommandHardwardRecv_ReadItems* pItems = (CommandHardwardRecv_ReadItems*) pCommandBase;
+                pItems->dataType = DataType_Accessory;
+                pItems->pojoList = pojoList;
+                m_pCommandHardwardEvent->onCommandHardwardRecv_ReadItems(pItems);
             }
-            
-            Json::Value json;
-            shared_ptr<vector<shared_ptr<Pojo>>> pojoList = AccessoryDao::readNestWithSQL(SQL);
-            
-            // 寫入至json輸出
-            Utility::pojoListToJson(inJsonObject, outJsonObject, pojoList);
-            
-            pCommandBase = new CommandHardwardRecv_ReadItems();
-            CommandHardwardRecv_ReadItems* pItems = (CommandHardwardRecv_ReadItems*) pCommandBase;
-            pItems->dataType = DataType_Accessory;
-            pItems->pojoList = pojoList;
-            m_pCommandHardwardEvent->onCommandHardwardRecv_ReadItems(pItems);
         }
-        
     }
     // 新增或修改
     else if (function.find("write") != std::string::npos) {
+        string SQL = "";
+        
         if (IfObject.isMember("AID")) {
-            Json::Value AIDArray = IfObject["AID"];
+            Json::Value jsonArray = IfObject["AID"];
             
             // 新增accessory, AID = [-1]
-            if (AIDArray.size() == 1 && AIDArray[0] == -1) {
+            if (jsonArray.size() == 1 && jsonArray[0] == -1) {
                 pCommandBase = new CommandHardwardRecv_CreateItems();
                 CommandHardwardRecv_CreateItems* pItems = (CommandHardwardRecv_CreateItems*) pCommandBase;
                 pItems->dataType = DataType_Accessory;
