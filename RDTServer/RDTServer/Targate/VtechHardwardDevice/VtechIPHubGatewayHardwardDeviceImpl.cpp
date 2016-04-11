749955dad7fa49c4567324b96094564216d76f73
jakey <jakey_lee@tutk.com>
2016-04-11 11:54:00 +0800

    修復編譯問題

--- RDTServer/RDTServer/Targate/VtechHardwardDevice/VtechIPHubGatewayHardwardDeviceImpl.cpp
+++ RDTServer/RDTServer/Targate/VtechHardwardDevice/VtechIPHubGatewayHardwardDeviceImpl.cpp
@@ -15,6 +15,12 @@
 #include "unixclientstream.hpp"
 #include "exception.hpp"
 
+// Database
+#include "AccessoryTypeEnum.hpp"
+#include "AccessoryDao.hpp"
+#include "ElementDao.hpp"
+#include "ElementNODao.hpp"
+
 #include <unistd.h>
 
 using libsocket::unix_stream_client; // need libsocket++
@@ -50,135 +56,132 @@ void* VtechIPHubGatewayHardwardImpl::socketInput(void *arg)
 {
    while(true)
    {
-       unsigned char buffer[BUFFER_SIZE];
-       memset(buffer, 0, BUFFER_SIZE);
+      unsigned char buffer[BUFFER_SIZE];
+      memset(buffer, 0, BUFFER_SIZE);
        
-       try {
-			sock.rcv(buffer,BUFFER_SIZE-1);
-// 			LOGD("we found received payload from unix socket = %s \n",buffer);
-			char* json = (char*) (buffer);
+      try {
+  			sock.rcv(buffer,BUFFER_SIZE-1);
+  			LOGD("we found received payload from unix socket = %s \n",buffer);
+  			char* json = (char*) (buffer);
     		LOGD("received json data:%s", json);
-			Json::Reader reader;
-			Json::Value inJsonObject;
-			if (reader.parse(json, inJsonObject))
-			{
-				LOGD("JSON object can be parsed");		
-				std::string operation= inJsonObject["operation"].asString();
-				LOGD("received json operation = %s", operation.c_str());
+  			Json::Reader reader;
+  			Json::Value inJsonObject;
+			
+//        if (reader.parse(json, inJsonObject))
+//  			{
+// 				LOGD("JSON object can be parsed");		
+// 				std::string operation= inJsonObject["operation"].asString();
+// 				LOGD("received json operation = %s", operation.c_str());
 
-				unsigned int accessoryId= inJsonObject["accessoryId"].asUInt();
-				LOGD("received json accessoryId = %d", accessoryId);
+// 				unsigned int accessoryId= inJsonObject["accessoryId"].asUInt();
+// 				LOGD("received json accessoryId = %d", accessoryId);
 
-				std::string functionCode= inJsonObject["functionCode"].asString();
-				LOGD("received json functionCode = %s", functionCode.c_str());
+// 				std::string functionCode= inJsonObject["functionCode"].asString();
+// 				LOGD("received json functionCode = %s", functionCode.c_str());
 
-				unsigned int value= inJsonObject["value"].asUInt();
-				LOGD("received json value = %d", value);
+// 				unsigned int value= inJsonObject["value"].asUInt();
+// 				LOGD("received json value = %d", value);
 
-				if (operation.compare("update") == 0)
-				{
-					LOGD("Vtech check we need to call the update below \n");
-/*
-					we should now call the update api below
-*/
+// 				if (operation.compare("update") == 0)
+// 				{
+// 					LOGD("Vtech check we need to call the update below \n");
+// /*
+// 					we should now call the update api below
+// */
 
-        // 通知update
-        FunctionCodeValueData* pFunctionCodeValueData = new FunctionCodeValueData();
-        pFunctionCodeValueData->value = value;
+//         // 通知update
+//         FunctionCodeValueData* pFunctionCodeValueData = new FunctionCodeValueData();
+//         pFunctionCodeValueData->value = value;
         
-        FunctionCodeData* pFunctionCodeData = new FunctionCodeData();
-        pFunctionCodeData->functonCode = functionCode.c_str();
-        pFunctionCodeData->functionCodeValueDataList.push_back(pFunctionCodeValueData);
+//         FunctionCodeData* pFunctionCodeData = new FunctionCodeData();
+//         pFunctionCodeData->functonCode = functionCode.c_str();
+//         pFunctionCodeData->functionCodeValueDataList.push_back(pFunctionCodeValueData);
 
-        AccessoryData* pAccessoryData = new AccessoryData();
-        pAccessoryData->accessoryId = accessoryId;
-        pAccessoryData->functionCodeDataList.push_back(pFunctionCodeData);
+//         AccessoryData* pAccessoryData = new AccessoryData();
+//         pAccessoryData->accessoryId = accessoryId;
+//         pAccessoryData->functionCodeDataList.push_back(pFunctionCodeData);
         
-        CommandHardwardSend_UpdateItems sendUpdateItems;
-        sendUpdateItems.baseDataList.push_back(pAccessoryData);
-        sendUpdateItems.dataType = DataType_Accessory;
-        sendUpdateItems.errorCode = 0;
+//         CommandHardwardSend_UpdateItems sendUpdateItems;
+//         sendUpdateItems.baseDataList.push_back(pAccessoryData);
+//         sendUpdateItems.dataType = DataType_Accessory;
+//         sendUpdateItems.errorCode = 0;
         
-        Device* pDevice = Device::getInstance();
-        JsonRDTCommand* jsonRDTCommand = (JsonRDTCommand*) pDevice->getCommand();
-        jsonRDTCommand->commandHardwardSend_UpdateItems(&sendUpdateItems);
-        /*
-        執行後在Server上看到
-        收到 hardward json = 
-        {
-           "operation" : "update",
-           "request" : {
-              "accessories" : [
-                 {
-                    "accessory_id" : 1,
-                    "accessory_type" : 0,
-                    "function_codes" : [
-                       {
-                          "functon_code" : "motion",
-                          "index" : 0,
-                          "value" : 1
-                       }
-                    ]
-                 }
-              ]
-           },
-           "serno" : 12345678,
-           "target" : "/accessory/"
-        }
-        */
+//         Device* pDevice = Device::getInstance();
+//         JsonRDTCommand* jsonRDTCommand = (JsonRDTCommand*) pDevice->getCommand();
+//         jsonRDTCommand->commandHardwardSend_UpdateItems(&sendUpdateItems);
+        
+//         執行後在Server上看到
+//         收到 hardward json = 
+//         {
+//            "operation" : "update",
+//            "request" : {
+//               "accessories" : [
+//                  {
+//                     "accessory_id" : 1,
+//                     "accessory_type" : 0,
+//                     "function_codes" : [
+//                        {
+//                           "functon_code" : "motion",
+//                           "index" : 0,
+//                           "value" : 1
+//                        }
+//                     ]
+//                  }
+//               ]
+//            },
+//            "serno" : 12345678,
+//            "target" : "/accessory/"
+//         }
+        
 
-				// FunctionCodeValueData* pFunctionCodeValueData = new FunctionCodeValueData();
-				// pFunctionCodeValueData->value = value;
+// 				// FunctionCodeValueData* pFunctionCodeValueData = new FunctionCodeValueData();
+// 				// pFunctionCodeValueData->value = value;
 				
-				// FunctionCodeData* pFunctionCodeData = new FunctionCodeData();
-				// pFunctionCodeData->functonCode = functionCode;
-				// pFunctionCodeData->functionCodeValueDataList.push_back(pFunctionCodeValueData);
+// 				// FunctionCodeData* pFunctionCodeData = new FunctionCodeData();
+// 				// pFunctionCodeData->functonCode = functionCode;
+// 				// pFunctionCodeData->functionCodeValueDataList.push_back(pFunctionCodeValueData);
 
-				// AccessoryData* pAccessoryData = new AccessoryData();
-				// pAccessoryData->accessoryId = accessoryId;
-				// pAccessoryData->functionCodeDataList.push_back(pFunctionCodeData);
+// 				// AccessoryData* pAccessoryData = new AccessoryData();
+// 				// pAccessoryData->accessoryId = accessoryId;
+// 				// pAccessoryData->functionCodeDataList.push_back(pFunctionCodeData);
 
-				// CommandBase* pCommand = NULL;
-				// pCommand = new CommandHardwardRecv_UpdateItems();
-				// CommandHardwardRecv_UpdateItems* pUpdateItems = (CommandHardwardRecv_UpdateItems*) pCommand;
-				// pUpdateItems->dataType = DataType_Accessory;
-				// pUpdateItems->baseDataList.push_back(pAccessoryData);
+// 				// CommandBase* pCommand = NULL;
+// 				// pCommand = new CommandHardwardRecv_UpdateItems();
+// 				// CommandHardwardRecv_UpdateItems* pUpdateItems = (CommandHardwardRecv_UpdateItems*) pCommand;
+// 				// pUpdateItems->dataType = DataType_Accessory;
+// 				// pUpdateItems->baseDataList.push_back(pAccessoryData);
 
-				// // 通知update
-				// LOGD("通知update");
-				// CommandHardwardSend_UpdateItems sendUpdateItems;
-				// sendUpdateItems.baseDataList = pUpdateItems->baseDataList;
-				// sendUpdateItems.dataType = pUpdateItems->dataType;
-				// sendUpdateItems.errorCode = pUpdateItems->errorCode;
+// 				// // 通知update
+// 				// LOGD("通知update");
+// 				// CommandHardwardSend_UpdateItems sendUpdateItems;
+// 				// sendUpdateItems.baseDataList = pUpdateItems->baseDataList;
+// 				// sendUpdateItems.dataType = pUpdateItems->dataType;
+// 				// sendUpdateItems.errorCode = pUpdateItems->errorCode;
 				
-				// Device* pDevice = Device::getInstance();
-				// JsonRDTCommand* jsonRDTCommand = (JsonRDTCommand*) pDevice->getCommand();
-				// jsonRDTCommand->commandHardwardSend_UpdateItems(&sendUpdateItems);
+// 				// Device* pDevice = Device::getInstance();
+// 				// JsonRDTCommand* jsonRDTCommand = (JsonRDTCommand*) pDevice->getCommand();
+// 				// jsonRDTCommand->commandHardwardSend_UpdateItems(&sendUpdateItems);
 
 
 
-				}
+// 				}
 
-				else if (operation.compare("reply") == 0)
-				{
-					LOGD("Vtech check we check for error code below \n");
-					ULEErrorCode = inJsonObject["errorCode"].asUInt();
-					LOGD("received json errorCode= %d", ULEErrorCode);
+// 				else if (operation.compare("reply") == 0)
+// 				{
+// 					LOGD("Vtech check we check for error code below \n");
+// 					ULEErrorCode = inJsonObject["errorCode"].asUInt();
+// 					LOGD("received json errorCode= %d", ULEErrorCode);
 
-				}
+// 				}
 
-				else
-					LOGD("Vtech check it's not a valid operation \n");
-
-				
-			}
-
-
-       }
-       catch (const libsocket::socket_exception& exc)
-       {
-           std::cerr << exc.mesg;
-       }
+// 				else
+// 					LOGD("Vtech check it's not a valid operation \n");
+//			   }
+      }
+      catch (const libsocket::socket_exception& exc)
+      {
+         std::cerr << exc.mesg;
+      }
    }
    
    return NULL;
@@ -227,14 +230,39 @@ void VtechIPHubGatewayHardwardImpl::onCommandHardwardRecv_CreateItem(CommandHard
     // 根據不同的DataType取得資料
     switch (pCommandHardwardRecv_CreateItems->dataType) {
         case DataType_Accessory: {
-            // 將新增資料填入
-            shared_ptr<vector<shared_ptr<Pojo>>> pAccessoryList = (shared_ptr<vector<shared_ptr<Pojo>>>) pCommandHardwardRecv_CreateItems->pojoList;
-            for (shared_ptr<Pojo> pPojo : *pAccessoryList) {
-                shared_ptr<AccessoryPojo>& accessoryPojo = (shared_ptr<AccessoryPojo>&) pPojo;
-                
-                LOGD("AID:%d", accessoryPojo->AID);
-                LOGD("IconType:%d", accessoryPojo->IconType);
-            }
+            /* 新增一筆資料 */
+            
+            // 建立accessoryPojo
+            shared_ptr<AccessoryPojo> pAccessoryPojo(new AccessoryPojo);
+            pAccessoryPojo->AID = 10;
+            pAccessoryPojo->Name = "PIR Sensor";
+            pAccessoryPojo->IconType = 1;
+            pAccessoryPojo->Connection = 1;
+            pAccessoryPojo->IsGateway = true;
+            
+            // 建立ElementPojo
+            shared_ptr<ElementPojo> pElement1(new ElementPojo);
+            pElement1->Element = "switch";
+            
+            // 建立ElementNOPojo
+            shared_ptr<ElementNOPojo> pNO1(new ElementNOPojo);
+            pNO1->ElementNO = 0;
+            pNO1->Value = "1";
+            pNO1->NtfyEnable = true;
+            
+            // 建立ElementNOPojo
+            shared_ptr<ElementNOPojo> pNO2(new ElementNOPojo);
+            pNO2->ElementNO = 1;
+            pNO2->Value = "100";
+            pNO2->NtfyEnable = true;
+            
+            // 放入相關階層
+            pAccessoryPojo->pSubPojoList->push_back(pElement1);
+            pElement1->pSubPojoList->push_back(pNO1);
+            pElement1->pSubPojoList->push_back(pNO2);
+            
+            // 將accessoryPojo放入pojoList
+            pCommandHardwardRecv_CreateItems->pojoList->push_back(pAccessoryPojo);
         }   break;
             
         default: {
@@ -254,7 +282,7 @@ void VtechIPHubGatewayHardwardImpl::onCommandHardwardRecv_DeleteItems(CommandHar
         case DataType_Accessory: {
             // remove pCommandHardwardRecv_DeleteItems->id;
         }   break;
-            
+        
         default: {
             LOGE("對應不到dataType");
         }   break;
@@ -270,15 +298,15 @@ void VtechIPHubGatewayHardwardImpl::onCommandHardwardRecv_ReadItems(CommandHardw
     switch (pCommandHardwardRecv_ReadItems->dataType) {
         case DataType_Accessory: {
             // 針對收到的資料做為參考
-            shared_ptr<vector<shared_ptr<Pojo>>> pAccessoryList = (shared_ptr<vector<shared_ptr<Pojo>>>) pCommandHardwardRecv_ReadItems->pojoList;
+            shared_ptr<vector<shared_ptr<Pojo>>> pAccessoryList = (shared_ptr<vector<shared_ptr<Pojo>>>) pCommandHardwardRecv_ReadItems->pPojoList;
             for (shared_ptr<Pojo> pPojo : *pAccessoryList) {
                 shared_ptr<AccessoryPojo>& accessoryPojo = (shared_ptr<AccessoryPojo>&) pPojo;
                 
                 LOGD("AID:%d", accessoryPojo->AID);
-                LOGD("IconType:%d", accessoryPojo->IconType);
+                LOGD("iconType:%d", accessoryPojo->IconType);
             }
         }   break;
-            
+        
         default: {
             LOGE("對應不到dataType");
         }   break;
@@ -288,61 +316,23 @@ void VtechIPHubGatewayHardwardImpl::onCommandHardwardRecv_ReadItems(CommandHardw
 void VtechIPHubGatewayHardwardImpl::onCommandHardwardRecv_UpdateItems(CommandHardwardRecv_UpdateItems* pCommandHardwardRecv_UpdateItems)
 {
     LOGD("onCommandHardwardRecv_UpdateItems");
-
-/*
-Vtech added on 14/3/2016
-*/
-	Json::Value root;
-	root["operation"] = "update";
     
     // 根據不同的DataType取得資料
     switch (pCommandHardwardRecv_UpdateItems->dataType) {
         case DataType_Accessory: {
-            // 針對收到的資料做為更新硬體參考
-            vector<AccessoryData*> accessoryList = (vector<AccessoryData*>&) pCommandHardwardRecv_UpdateItems->baseDataList;
-            
-            for (int i=0 ; i<accessoryList.size() ; i++) {
-                LOGD("accessoryId:%d", accessoryList[i]->accessoryId);
-				root["accessoryId"] = accessoryList[i]->accessoryId;
+            // 針對收到的資料做為參考
+            shared_ptr<vector<shared_ptr<Pojo>>> pAccessoryList = (shared_ptr<vector<shared_ptr<Pojo>>>) pCommandHardwardRecv_UpdateItems->pPojoList;
+            for (shared_ptr<Pojo> pPojo : *pAccessoryList) {
+                shared_ptr<AccessoryPojo>& accessoryPojo = (shared_ptr<AccessoryPojo>&) pPojo;
                 
-                for (int j=0 ; j<accessoryList[i]->functionCodeDataList.size() ; j++) {
-                    LOGD("functionCode:%s", accessoryList[i]->functionCodeDataList[j]->functonCode.c_str());
-
-					root["functionCode"] = accessoryList[i]->functionCodeDataList[j]->functonCode.c_str();
-                    
-                    for (int k=0 ; k<accessoryList[i]->functionCodeDataList[j]->functionCodeValueDataList.size() ; k++) {
-                        LOGD("value:%d", accessoryList[i]->functionCodeDataList[j]->functionCodeValueDataList[k]->value);
-
-					root["value"] = accessoryList[i]->functionCodeDataList[j]->functionCodeValueDataList[k]->value;
-
-                    }
-                }
+                LOGD("AID:%d", accessoryPojo->AID);
+                LOGD("iconType:%d", accessoryPojo->IconType);
+                // Update資料
             }
-		std::string json = root.toStyledString();
-		LOGD("Received JSON string in UpdateItems - jsonString:%s", json.c_str());
-		sendToGateway((char*)json.c_str(), json.length());
-		while (ULEErrorCode == -999)
-			usleep(1);
-		pCommandHardwardRecv_UpdateItems->errorCode = ULEErrorCode;
-		ULEErrorCode = -999;
         }   break;
-            
+        
         default: {
             LOGE("對應不到dataType");
         }   break;
     }
-
-
-#if 0
-    // 通知update
-    LOGD("通知update");
-    CommandHardwardSend_UpdateItems sendUpdateItems;
-    sendUpdateItems.baseDataList = pCommandHardwardRecv_UpdateItems->baseDataList;
-    sendUpdateItems.dataType = pCommandHardwardRecv_UpdateItems->dataType;
-    sendUpdateItems.errorCode = pCommandHardwardRecv_UpdateItems->errorCode;
-    
-    Device* pDevice = Device::getInstance();
-    JsonRDTCommand* jsonRDTCommand = (JsonRDTCommand*) pDevice->getCommand();
-    jsonRDTCommand->commandHardwardSend_UpdateItems(&sendUpdateItems);
-#endif
 }
