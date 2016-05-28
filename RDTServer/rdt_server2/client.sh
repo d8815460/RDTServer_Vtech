93216796adae5fc1455e53c64f5ff3dba8d00fa5
eddyc1119@gmail.com <eddyc1119@gmail.com>
2016-05-28 15:20:42 +0800

    change to use parameter UID

--- RDTServer/rdt_server2/client.sh
+++ RDTServer/rdt_server2/client.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 echo "start client emulator"
-cd kalay_dserver
+cd kalay_dserver 
 #make
-./kalay_dserver
+./kalay_dserver DRFTBHN6XXUUVM6KWTE1
 
--- RDTServer/rdt_server2/kalay_dserver/kalay_dserver.cpp
+++ RDTServer/rdt_server2/kalay_dserver/kalay_dserver.cpp
@@ -4,13 +4,23 @@
 
 #include "dtable.h"
 
+// char *__testUID="DRFTBHN6XXUUVM6KWTE1";
+
 int main(int argc,char *argv[])
 {
 	//__allObjects.dump();
+	//__allObjects.dumpByLocation();
 
-	__allObjects.dumpByLocation();
+	if ( argc != 2 )
+	{
+		printf("miss UID\n");
+		printf("usage './kalay_dserver UID'\n");
+		exit(0);
+	}
 
-	kalay_device_server_agent_start((char*)"DRFTBHN6XXUUVM6KWTE1",(char*) "/tmp/unixsocket");
+	printf("UID:%s\n",argv[1]);
+
+	kalay_device_server_agent_start((char*)argv[1],(char*) "/tmp/unixsocket");
 
 	return 0;
 }
